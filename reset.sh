b77f0acd77d6a88064978b506ca5b66ffbaf6d2d
freddieRv <federico.ram.v@gmail.com>
2019-01-06 20:20:44 +0100

    Added mini bash script to delete aux files

---
+++ reset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm paper/evolutive_algorithms.aux
+rm paper/evolutive_algorithms.bbl
+rm paper/evolutive_algorithms.blg
+rm paper/evolutive_algorithms.log
+rm paper/evolutive_algorithms.out
